edd1eb7f182009afc69944ede70492610a9d6f21
Joshua Henderson <hendersonjosh97@gmail.com>
2018-05-02 14:36:03 +0100

    Update generatescene.js

--- generatescene.js
+++ generatescene.js
@@ -85,8 +85,8 @@ AFRAME.registerComponent('movement-listener', {
     this.onGripDown = this.onGripDown.bind(this);
     this.onTriggerDown = this.onTriggerDown.bind(this);
     // Set the event listeners for each button event
-    el.addEventListener('pistolstart', this.onTriggerUp);
-    el.addEventListener('pistolend', this.onTriggerDown);
+    el.addEventListener('thumbup', this.onTriggerUp);
+    el.addEventListener('thumbdown', this.onTriggerDown);
     el.addEventListener('gripup', this.onGripUp);
     el.addEventListener('gripdown', this.onGripDown);
   },
